56da58e2ef73f0afef10d742e7725d1df5be0012
Sarah Greene <sarah2000greene@gmail.com>
2022-02-18 20:47:25 -0500

    Install necessary packages and create basic set up for project

---
+++ main.py
@@ -0,0 +1,20 @@
+import math
+import pandas as panda
+from matplotlib import pyplot
+import seaborn
+import gmplot
+
+
+# Find the latitude/longitude coordinates for the area of interest
+def get_coordinates():
+     #read in some data
+
+
+# Represents the busiest zones of traffic for uber drivers
+def create_heat_map():
+    print('bye')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    create_heat_map()
