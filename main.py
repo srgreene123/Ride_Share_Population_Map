4cc44a4031428ceba6467dee7c3b6b3250d0d31d
Sarah Greene <sarah2000greene@gmail.com>
2022-02-19 10:54:15 -0500

    Get coordinates of region from specified raw data filed

--- main.py
+++ main.py
@@ -7,35 +7,34 @@ import gmplot
 
 # Find the latitude/longitude coordinates for the area of interest
 def get_coordinates():
-    file_info = pandas.read_csv('')  # figure out how to get public data
-    # parse file based on coordinates only
-    refined_results = file_info.parse()
-
-    # select specific sample size to choose from
-    refined_results = refined_results.sample(n=2000)
-
+    # grab public data from https://github.com/fivethirtyeight/uber-tlc-foil-response
+    file_info = pandas.read_csv('uber-raw-data-may14.csv')
+    # parse csv file based on longitude/latitude only
+    refined_results = file_info.drop(columns=['Date/Time', 'Base'])
+    # select specific sample size to choose from since there are many data points
+    refined_results = refined_results.sample(n=30000)
     # plot the coordinate ranges
-    coordinate_plot = seaborn.relplot(data=refined_results, kind='scatter', x='latitude', y='longitude')
+    coordinate_plot = seaborn.relplot(data=refined_results, kind='scatter', x='Lat', y='Lon')
     return coordinate_plot
 
 
 # Find optimal distance between clusters
 def find_minimum_distance(coordinates):
-    print('hi2')
     return 1
 
 
 # Find the needed clusters
 def find_clusters():
-    print('hi3')
+    return 2
 
 
-# Create graphical representation of the busiest zones with people for rideshare drivers
+# Create graphical representation of the busiest zones with people for ride-share drivers
 def create_heat_map():
-    coordinates_area_rep = get_coordinates()
-    optimal_dist = find_minimum_distance(coordinates_area_rep)
+    return 3
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    create_heat_map()
+    # create_heat_map()
+    get_coordinates()
+
