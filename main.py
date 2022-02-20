386d4fbb1ad32192c2ef7fdceaf23ebe7ef22a01
Sarah Greene <sarah2000greene@gmail.com>
2022-02-19 20:09:31 -0500

    Wrote function to graph the distances between neighbors, working on returning the optimal epsilon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
+
+import numpy
 import pandas
 from pandas.core.frame import DataFrame
 from matplotlib import pyplot
@@ -6,6 +8,7 @@ import seaborn
 import gmplot
 from sklearn import preprocessing
 from sklearn.cluster import DBSCAN
+from sklearn.neighbors import NearestNeighbors
 
 
 # Find the latitude/longitude coordinates for the area of interest
@@ -27,30 +30,28 @@ def get_coordinates():
 def find_generalized_clusters(general_area_coordinates):
     normalization_prep = preprocessing.StandardScaler()  # prepares standardization on coordinates
     standardized_data = normalization_prep.fit_transform(general_area_coordinates.values)
-
-    # general_area_coordinates.head(n=2)
-
     return standardized_data
+
+
+# Find the max distance between clusters to be considered as neighbors
+def find_epsilon(general_region):
+    # specified num of clusters to compare and computed the nearest neighbors
+    values_between = NearestNeighbors(n_neighbors=2).fit(general_region.values)
+    distance, index = values_between.kneighbors(general_region.values)
+    distance = numpy.sort(distance, axis=0)
+    distance = distance[:,1][29500:] # to ensure the curve appears
+    pyplot.plot(distance)
+    pyplot.show()
+    max_value =
+    return max_value
 
 
 # Find the high density clusters (most populous area)
 def find_high_pop_clusters(general_area, standardized_data):
-    epsilon = 0.35  # max distance between clusters to be considered neighbors (arbitrary value)
+    find_epsilon(general_area)  # max distance between clusters to be considered neighbors (arbitrary value)
 
     # use DBSCAN algorithm to compare the high density vs low density area in terms of population
     # clusters = DBSCAN(eps=epsilon, min_samples=20).fit(general_area.values)
-
-    predict_clusters = DBSCAN(eps=epsilon, min_samples=20).fit_predict(standardized_data)  # create high density cluster predictions based on normalized data
-    created_clusters = pandas.Series(data=predict_clusters)  #
-    created_clusters.unique()  # create unique clusters so no duplicates are allowed
-
-    general_area['Populous_predictions'] = created_clusters.values
-
-    seaborn.scatterplot(data=general_area, hue='Populous_predictions', x='Lat', y='Lon', palette='Set2')
-    pyplot.show()  # display plot based on high density clusters
-
-    seaborn.scatterplot(data=general_area[general_area.Populous_predictions == 0], hue='Populous_predictions', x='Lat', y='Lon', palette='Set2')
-    pyplot.show()
 
 
 # Create graphical representation of the busiest zones with people for ride-share drivers
@@ -59,13 +60,14 @@ def create_heat_map():
     standardized_data = find_generalized_clusters(general_region)
     general_area = pandas.DataFrame(general_region)
     general_area.head(n=2)
-    find_high_pop_clusters(general_area, standardized_data)
+    general_area = find_high_pop_clusters(general_area, standardized_data)
 
-    longitude_list = list(general_region.Lon.values)
-    latitude_list = list(general_region.Lat.values)
+    longitude_list = list(general_area.Lon.values)
+    latitude_list = list(general_area.Lat.values)
 
     # visualize as a heat map through Google Maps
     google_map = gmplot.GoogleMapPlotter.from_geocode('Manhattan, New York')
+    google_map.apikey = 'hackathonSecretKey'
     google_map.heatmap(latitude_list, longitude_list)
     google_map.draw('~/Users/Sarah/Desktop/map1.html')
 
